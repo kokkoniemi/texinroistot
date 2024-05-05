92e84b910ff6b72a1584d6fa3b9fc4d2699a003d
kokkoniemi <mikko@kokkoniemi.com>
2024-05-05 16:19:50 +0300

    importer stuff & documented db structure

---
+++ texinroistot-server/internal/db/schema.sql
@@ -0,0 +1,148 @@
+-- This script documents the database structure. It can also be used to create the tables in an empty database 
+
+-- AUTHORS:
+
+-- Table Definition
+CREATE TABLE "public"."authors" (
+	    "id" int8 NOT NULL,
+	    "first_name" varchar,
+	    "last_name" varchar,
+	    "is_writer" bool,
+	    "is_drawer" bool,
+	    "is_inventor" bool,
+	    "version" int8 NOT NULL,
+	    "hash" varchar NOT NULL,
+	    PRIMARY KEY ("id")
+);
+
+-- Column Comment
+COMMENT ON COLUMN "public"."authors"."hash" IS 'consistent identifier between different versions';
+
+
+DROP TYPE IF EXISTS "public"."author_type";
+CREATE TYPE "public"."author_type" AS ENUM ('writer', 'drawer', 'inventor');
+
+-- Table Definition
+CREATE TABLE "public"."authors_in_stories" (
+	    "id" int8 NOT NULL,
+	    "story" int8,
+	    "author" int8,
+	    "type" "public"."author_type",
+	    PRIMARY KEY ("id")
+);
+
+-- Comments
+COMMENT ON TABLE "public"."authors" IS 'An author of a story. Can be writer, drawer, or story inventor';
+
+
+-- STORIES:
+
+-- Table Definition
+CREATE TABLE "public"."stories" (
+	    "id" int8 NOT NULL,
+	    "order_num" int8,
+	    "version" int8 NOT NULL,
+	    "hash" varchar NOT NULL,
+	    PRIMARY KEY ("id")
+);
+
+-- Column Comment
+COMMENT ON COLUMN "public"."stories"."hash" IS 'consistent identifier between different versions';
+
+-- Table Definition
+CREATE TABLE "public"."stories_in_publications" (
+	    "id" int8 NOT NULL,
+	    "story" int8 NOT NULL,
+	    "publication" int8 NOT NULL,
+	    "title" varchar NOT NULL,
+	    PRIMARY KEY ("id")
+);
+
+DROP TYPE IF EXISTS "public"."publication_type";
+CREATE TYPE "public"."publication_type" AS ENUM ('perus', 'maxi', 'suur', 'muu_erikois', 'kronikka', 'kirjasto', 'italia_perus', 'italia_erikois');
+
+-- Table Definition
+CREATE TABLE "public"."publications" (
+	    "id" int8 NOT NULL,
+	    "type" "public"."publication_type",
+	    "year" int8,
+	    "issue" varchar NOT NULL,
+	    "version" int8 NOT NULL,
+	    "hash" varchar NOT NULL,
+	    PRIMARY KEY ("id")
+);
+
+-- Column Comment
+COMMENT ON COLUMN "public"."publications"."hash" IS 'consistent identifier between different versions';
+
+
+-- USERS:
+
+-- Table Definition
+CREATE TABLE "public"."users" (
+	    "id" int8 NOT NULL,
+	    "created_at" timestamptz NOT NULL DEFAULT now(),
+	    "hash" varchar NOT NULL,
+	    "is_admin" bool NOT NULL DEFAULT false,
+	    PRIMARY KEY ("id")
+);
+
+
+-- VERSIONS:
+
+-- Table Definition
+CREATE TABLE "public"."versions" (
+	    "id" int8 NOT NULL,
+	    "created_at" timestamptz NOT NULL DEFAULT now(),
+	    "is_active" bool NOT NULL DEFAULT false,
+	    PRIMARY KEY ("id")
+);
+
+-- Comments
+COMMENT ON TABLE "public"."versions" IS 'Every row in database is related to certain version';
+
+
+-- VILLAINS:
+
+-- Table Definition
+CREATE TABLE "public"."villains" (
+	    "id" int8 NOT NULL,
+	    "ranks" _varchar,
+	    "first_names" _varchar,
+	    "last_name" varchar,
+	    "version" int8 NOT NULL,
+	    "hash" varchar NOT NULL,
+	    PRIMARY KEY ("id")
+);
+
+-- Column Comment
+COMMENT ON COLUMN "public"."villains"."hash" IS 'consistent identifier between different versions';
+
+
+-- Table Definition
+CREATE TABLE "public"."villains_in_stories" (
+	    "id" int8 NOT NULL,
+	    "villain" int8 NOT NULL,
+	    "story" int8 NOT NULL,
+	    "hash" varchar NOT NULL,
+	    "nicknames" _varchar,
+	    "aliases" _varchar,
+	    "destiny" text,
+	    PRIMARY KEY ("id")
+);
+
+
+-- FOREIGN KEY & INDICE DEFINITIONS
+ALTER TABLE "public"."authors" ADD FOREIGN KEY ("version") REFERENCES "public"."versions"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."authors_in_stories" ADD FOREIGN KEY ("story") REFERENCES "public"."stories"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."authors_in_stories" ADD FOREIGN KEY ("author") REFERENCES "public"."authors"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."publications" ADD FOREIGN KEY ("version") REFERENCES "public"."versions"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."stories" ADD FOREIGN KEY ("version") REFERENCES "public"."versions"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."stories_in_publications" ADD FOREIGN KEY ("story") REFERENCES "public"."stories"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."stories_in_publications" ADD FOREIGN KEY ("publication") REFERENCES "public"."publications"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."villains" ADD FOREIGN KEY ("version") REFERENCES "public"."versions"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."villains_in_stories" ADD FOREIGN KEY ("story") REFERENCES "public"."stories"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."villains_in_stories" ADD FOREIGN KEY ("villain") REFERENCES "public"."villains"("id") ON DELETE CASCADE;
+
+CREATE UNIQUE INDEX users_hash_key ON public.users USING btree (hash)
+
--- texinroistot-server/internal/importer/villains.go
+++ texinroistot-server/internal/importer/villains.go
@@ -1,6 +1,9 @@
 package importer
 
 import (
+	"fmt"
+	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/kokkoniemi/texinroistot/internal/crypt"
@@ -20,8 +23,74 @@ type importerStoryVillain struct {
 	item    *db.StoryVillain
 }
 
+func (i *importer) getVillainIndex(villainID int64) int {
+	return slices.IndexFunc(i.villains, func(v *importerVillain) bool {
+		return v.externalVillainID == villainID
+	})
+}
+
+func (i *importer) hasVillain(villainID int64) bool {
+	return i.getVillainIndex(villainID) != -1
+}
+
+func (i *importer) getVillain(villainID int64) *importerVillain {
+	idx := i.getVillainIndex(villainID)
+	if idx != -1 {
+		return i.villains[idx]
+	}
+	return nil
+}
+
+func (i *importer) addVillain(villain *db.Villain, villainID int64) *importerVillain {
+	i.totalEntities++
+
+	importerVillain := &importerVillain{
+		ID:                id(i.totalEntities),
+		externalVillainID: villainID,
+		item:              villain,
+	}
+	i.villains = append(i.villains, importerVillain)
+
+	return importerVillain
+}
+
+func (i *importer) getStoryVillainIndex(villainID id, storyID id) int {
+	return slices.IndexFunc(i.storyVillains, func(sv *importerStoryVillain) bool {
+		return sv.villain == villainID && sv.story == storyID
+	})
+}
+
+func (i *importer) hasStoryVillain(villainID id, storyID id) bool {
+	return i.getStoryVillainIndex(villainID, storyID) != -1
+}
+
+func (i *importer) getStoryVillain(villainID id, storyID id) *importerStoryVillain {
+	idx := i.getStoryVillainIndex(villainID, storyID)
+	if idx != -1 {
+		return i.storyVillains[idx]
+	}
+	return nil
+}
+
+func (i *importer) addStoryVillain(storyVillain *db.StoryVillain, villainID id, storyID id) *importerStoryVillain {
+	i.totalEntities++
+
+	importerStoryVillain := &importerStoryVillain{
+		ID:      id(i.totalEntities),
+		story:   storyID,
+		villain: villainID,
+		item:    storyVillain,
+	}
+	i.storyVillains = append(i.storyVillains, importerStoryVillain)
+
+	return importerStoryVillain
+}
+
 func (i *importer) importVillain(storyID id, r row) {
-	villainID := strings.TrimSpace(r.getValue("villain_id"))
+	villainID, err := strconv.ParseInt(strings.TrimSpace(r.getValue("villain_id")), 10, 64)
+	if err != nil {
+		villainID = -1
+	}
 	villainRanks := i.TrimmedSplit(r.getValue("ranks"), ";")
 	firstNames := i.TrimmedSplit(r.getValue("first_names"), ";")
 	lastName := r.getValue("last_name")
@@ -33,8 +102,8 @@ func (i *importer) importVillain(storyID id, r row) {
 	var villain *importerVillain
 
 	createHash := func() string {
-		if len(villainID) > 0 {
-			return crypt.Hash(villainID)
+		if villainID != -1 {
+			return crypt.Hash(strconv.FormatInt(villainID, 10))
 		}
 		return crypt.Hash(strings.Join(firstNames, "") +
 			lastName +
@@ -44,22 +113,67 @@ func (i *importer) importVillain(storyID id, r row) {
 			strings.Join(destiny, ""))
 	}
 
-	if !i.hasVillain(villainID) {
+	// create villain if not exist
+	if villainID == -1 || !i.hasVillain(villainID) {
 		villain = i.addVillain(&db.Villain{
 			Hash:       createHash(),
-			Ranks:      ranks,
+			Ranks:      villainRanks,
 			FirstNames: firstNames,
-			LastName:   lastName})
+			LastName:   lastName,
+		}, villainID)
 	} else {
+		// update existing villain with row info
 		villain = i.getVillain(villainID)
-		// check if villain has rank, fistNames and lastNames
-		// and add those if they're missing
+
+		for _, rank := range villainRanks {
+			if !slices.Contains(villain.item.Ranks, rank) {
+				villain.item.Ranks = append(villain.item.Ranks, rank)
+			}
+		}
+
+		for _, name := range firstNames {
+			if !slices.Contains(villain.item.FirstNames, name) {
+				villain.item.FirstNames = append(villain.item.FirstNames, name)
+			}
+		}
+
+		if len(villain.item.LastName) == 0 && len(lastName) > 0 {
+			villain.item.LastName = lastName
+		}
 
 	}
 
-	// check if importerStoryVillain exists and load
-	// 	else create empty storyVillain
+	var storyVillain *importerStoryVillain
 
-	// check if storyVillain has nicknames, aliases, roles
-	// and desiny and add those if they're missing
+	// create storyVillain if not exist
+	if !i.hasStoryVillain(villain.ID, storyID) {
+		storyVillain = i.addStoryVillain(&db.StoryVillain{
+			Hash:      crypt.Hash(fmt.Sprintf("%d%d", villain.ID, storyID)),
+			Nicknames: nicknames,
+			Aliases:   aliases,
+			Roles:     roles,
+			Destiny:   destiny,
+		}, villain.ID, storyID)
+	} else {
+		// update existing storyVillain with row info
+		storyVillain = i.getStoryVillain(villain.ID, storyID)
+
+		for _, nick := range nicknames {
+			if !slices.Contains(storyVillain.item.Nicknames, nick) {
+				storyVillain.item.Nicknames = append(storyVillain.item.Nicknames, nick)
+			}
+		}
+
+		for _, alias := range aliases {
+			if !slices.Contains(storyVillain.item.Aliases, alias) {
+				storyVillain.item.Aliases = append(storyVillain.item.Aliases, alias)
+			}
+		}
+
+		for _, role := range roles {
+			if !slices.Contains(storyVillain.item.Roles, role) {
+				storyVillain.item.Roles = append(storyVillain.item.Roles, role)
+			}
+		}
+	}
 }
