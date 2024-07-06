274825cd6769c89d7800a556d3d6498bf492a652
kokkoniemi <mikko@kokkoniemi.com>
2024-07-06 16:37:09 +0300

    wip

--- texinroistot-ui/src/routes/tarinat/+page.svelte
+++ texinroistot-ui/src/routes/tarinat/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import { writable } from 'svelte/store'
+	import { setContext } from 'svelte'; 
 	import type { PageData } from './$types';
 	export let data: PageData;
-	console.log(data);
+
+	type TStory = {
+		orderNumber: number;
+	};
+
+	const stories = writable<TStory[]>();
+	$: stories.set(data.stories);
+	setContext('stories', stories);
 </script>
 
 <h1>Tarinat</h1>
+
+{#each $stories as story}
+<hr />
+	<h3>{story.orderNumber}</h3>
+<hr />
+{/each}
