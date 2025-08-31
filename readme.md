940d3050aa641ad1ab485e766220d7377381efb7
kokkoniemi <mikko@kokkoniemi.com>
2025-08-31 15:15:17 +0300

    fix connection from frontend to backend api

--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 # Project
 
 * This is a comic catalog project for the villains in Tex Willer series.
+
+## Startup
+
+Copy .env-example to .env and populate values
+
+```
+docker-compose up
+psql -h localhost -p 5432 -d tex -U tex < texinroistot-server/internal/db/schema.sql
+docker-compose exec backend go run cmd/importer/importer.go
+```
--- texinroistot-ui/src/routes/api/tarinat/+server.ts
+++ texinroistot-ui/src/routes/api/tarinat/+server.ts
@@ -1,8 +1,9 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { BACKEND_HOST } from '$env/static/private';
 
 export const GET: RequestHandler = async ({ url, fetch }) => {
-	const res = await fetch("http://localhost:6969/api/stories");
+	const res = await fetch(`${BACKEND_HOST}/api/stories`);
 	const stories = await res.json();
 	
 	return json(stories);
